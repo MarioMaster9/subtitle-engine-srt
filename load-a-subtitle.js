badd7e352867d5f5c2af5d0898523445fc4a40cd
MarioMaster9 <73719966+MarioMaster9@users.noreply.github.com>
2026-01-22 21:32:51 -0500

    Create load-a-subtitle.js

---
+++ load-a-subtitle.js
@@ -0,0 +1,32 @@
+let videoTag = document.getElementsByTagName("video")[0];
+
+document.getElementById("subtitleDiv").innerHTML = `<style>
+.subContainer {
+	font-family: Arial, Helvetica, sans-serif;
+}
+.subContainer {
+	line-height: 1em;
+	font-weight: bold;
+	font-size: 3em;
+	text-align: center;
+	vertical-align: text-bottom;
+	text-shadow: #000 0px 0px 1px;
+	-webkit-text-stroke: 2px black;
+	color: white;
+	margin-top: 15em;
+}
+</style>
+`;
+
+window.askSubtitles();
+
+videoTag.addEventListener("timeupdate", (event) => {
+	let timePosition = videoTag.currentTime;
+	window.subtitleElements.forEach((item, index) => {
+		if (window.subtitleList[index].start < timePosition && window.subtitleList[index].end >= timePosition) {
+			item.style.display = "";
+		} else {
+			item.style.display = "none";
+		}
+	});
+});
